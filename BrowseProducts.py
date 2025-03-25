bec85ad60031421fa008f2c9b68d8999781b51ca
malybaryl <kacper_barylowicz@wp.pl>
2025-03-26 00:49:22 +0100

    scrollable BrowseProducts.py

--- BrowseProducts.py
+++ BrowseProducts.py
@@ -6,8 +6,14 @@ class BrowseProducts(ctk.CTkFrame):
         super().__init__(parent)
         self.configure(fg_color="#242424")
         self.perm_level = parent.root.get_perm_level()
-        self.product_list = self.generate_products_list()
         self.parent = parent
+
+        # Tworzenie przewijalnego kontenera
+        self.scroll_frame = ctk.CTkScrollableFrame(self)
+        self.scroll_frame.configure(fg_color="#242424")
+        self.scroll_frame.pack(expand=True, fill="both", padx=10, pady=10)
+        
+        self.product_list = self.generate_products_list()
         self.create_products_list()
     
     def generate_products_list(self):
@@ -17,7 +23,7 @@ class BrowseProducts(ctk.CTkFrame):
 
     def create_products_list(self):
         for product in self.product_list:
-            product.pack(padx = 2, pady = 2, side = 'left')
+            product.pack(in_=self.scroll_frame, padx=2, pady=2, side='top', fill='x')
 
     def unpack_product_by_id(self, product_id):
         for product in self.product_list:
@@ -25,5 +31,5 @@ class BrowseProducts(ctk.CTkFrame):
                 product.pack_forget()
     
     def add_product(self, product):
-        product.pack(pady = 2, expand = True, fill = 'x')
+        product.pack(in_=self.scroll_frame, pady=2, expand=True, fill='x')
         self.product_list.append(product)
